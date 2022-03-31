bc9068144ddf7f4d28ea8b536ea4a9b5f8cf3c77
Brett Seifried <seifriedbrett@gmail.com>
2022-03-31 10:17:24 -0700

    wrote test for redirect to github OAuth login page, started controller and linked in app.js

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Gitty
+
+## Logging in
+
+[] log in via .post
+[] log out via .delete
+[] authentication through middleware
+[] cookie to store data
+[] JWTs to store data
+[] sign & verfiy JWT
+[] GitHub OAuth for authentication
+
+## Posting
+
+[] limit 225 characters
+[] protected post
+[] protected to get posts
--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,12 @@ describe('alchemy-app routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('should redirect to github OAuth page upon logging in', async () => {
+    const req = await request(app).get('/api/v1/auth/login');
+
+    expect(req.header.location).toMatch(
+      '/https://github.com/login/oauth/authorize?client_id=[wd]+&scope=user&redirect_uri=http://localhost:7890/api/v1/github/login/callback'
+    );
+  });
 });
